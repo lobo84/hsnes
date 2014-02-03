e95c4d74bab087b828e547de4c1d0e592ccc431a
Niclas Ericson <niclas.ericson@gmail.com>
2014-02-03 02:38:48 +0100

    runs nestest rom

--- Main.hs
+++ Main.hs
@@ -1,14 +1,19 @@
 import qualified Data.ByteString.Lazy as L
+
 import Rom as R
+import Cpu as C
 
 main :: IO ()
 main = do 
-  bytes <- L.readFile "megaman.nes"
+  bytes <- L.readFile "nestest.nes"
   case R.parse(bytes) of 
     Left errMsg -> putStr ("Parse failed:\n" ++ errMsg)
-    Right rom -> putStr ("Parse success:\n" ++ (romInfo rom))
+    Right rom -> C.runCpuInteractive (C.initCpu 0 intRom [])
+      where intRom = map (\v -> fromIntegral(v)::Int) (L.unpack(R.prgData rom))
+      --putStr ("Parse success:\n" ++ (romInfo rom))
                         
   
 romInfo :: R.Rom -> String                        
-romInfo rom = "Program size = " ++ prgSize  
+romInfo rom = "Program size = " ++ prgSize ++ "\n" ++ "Rom data = " ++ romData ++ "\n"
   where prgSize = show (R.sizePrgRom (R.header rom))
+        romData = show (R.prgData rom)
